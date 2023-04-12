aacf26d1394041f07147f4a2ddd526567fd1077d
sotenck <z8F-2fX;~#8>
2023-04-12 17:22:38 +0200

    tried implementing Scrren glow effect - not functional yet

--- libs/imageFusion.py
+++ libs/imageFusion.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 import os
 
 class TransparentImageOverlay:
@@ -12,7 +12,7 @@ class TransparentImageOverlay:
     def overlay_images(self, output_path, keepScreenshot=False):
         # Öffne die beiden Bilder und konvertiere sie in RGBA-Modus, um Transparenz zu unterstützen
         with Image.open(self.bottom_image_path) as b_image, Image.open(self.top_image_path) as t_image:
-            bottom_image = b_image.convert('RGBA')
+            background = b_image.convert('RGBA')
             top_image = t_image.convert('RGBA')
 
             # Berechne die Höhe des oberen Bildes basierend auf dem Seitenverhältnis und der Breite
@@ -22,14 +22,27 @@ class TransparentImageOverlay:
             # Skaliere das obere Bild auf die gewünschte Größe
             top_image = top_image.resize((self.width, height))
 
-            # Erstellen eines transparenten Hintergrundbildes, das der Größe des unteren Bildes entspricht
-            background = Image.new('RGBA', bottom_image.size, (255, 255, 255, 0))
 
-            # Einfügen des unteren Bildes in den Hintergrund
-            background.paste(bottom_image, (0, 0))
+            # Calculate the size of the mask with a 10% margin around the smaller image
+            margin_width = top_image.width // 10
+            margin_height = top_image.height // 10
+            mask_width = top_image.width + margin_width * 2
+            mask_height = top_image.height + margin_height * 2
+            mask_size = (mask_width, mask_height)
+
+            alpha = top_image.split()[-1]
+            alpha_blur = alpha.filter(ImageFilter.GaussianBlur(radius=10))
+            alpha_blur_large = alpha_blur.resize(mask_size, resample=Image.BOX)
+
+            # Create a mask from the larger blurred alpha channel that matches the size of the smaller image
+            maskImg = Image.new('RGBA', top_image.size, (0, 0, 0, 0))
+            maskImg.paste(alpha_blur_large, (-margin_width, -margin_height))
+
+            # Einfügen des Screen Glows
+            background.paste(top_image, (self.x, self.y), mask=maskImg)
 
             # Einfügen des oberen Bildes an der gewünschten Position auf dem Hintergrund
-            background.paste(top_image, (self.x, self.y), mask=top_image)
+            # background.paste(top_image, (self.x, self.y), mask=top_image)
 
             # Speichern des neuen Bildes
             background.save(output_path)
