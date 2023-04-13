18104f6622fccfbb997e1b0a6442d2d2862be9d3
sotenck <z8F-2fX;~#8>
2023-04-13 11:59:33 +0200

    added screen glow effekt - may need to be altered to blur with enought px

--- libs/imageFusion.py
+++ libs/imageFusion.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageDraw, ImageFilter
 import os
 
 class TransparentImageOverlay:
@@ -13,42 +13,45 @@ class TransparentImageOverlay:
         # Öffne die beiden Bilder und konvertiere sie in RGBA-Modus, um Transparenz zu unterstützen
         with Image.open(self.bottom_image_path) as b_image, Image.open(self.top_image_path) as t_image:
             background = b_image.convert('RGBA')
-            top_image = t_image.convert('RGBA')
+            screenshot = self.transformScreenshot(t_image).convert('RGBA')
 
-            # Berechne die Höhe des oberen Bildes basierend auf dem Seitenverhältnis und der Breite
-            width_percent = (self.width / float(top_image.size[0]))
-            height = int((float(top_image.size[1]) * float(width_percent)))
+            # Place white blur rectangle
+            backgroundComp = self.add_blur(background, screenshot, 100, 0.3)
 
-            # Skaliere das obere Bild auf die gewünschte Größe
-            top_image = top_image.resize((self.width, height))
-
-
-            # Calculate the size of the mask with a 10% margin around the smaller image
-            margin_width = top_image.width // 10
-            margin_height = top_image.height // 10
-            mask_width = top_image.width + margin_width * 2
-            mask_height = top_image.height + margin_height * 2
-            mask_size = (mask_width, mask_height)
-
-            alpha = top_image.split()[-1]
-            alpha_blur = alpha.filter(ImageFilter.GaussianBlur(radius=10))
-            alpha_blur_large = alpha_blur.resize(mask_size, resample=Image.BOX)
-
-            # Create a mask from the larger blurred alpha channel that matches the size of the smaller image
-            maskImg = Image.new('RGBA', top_image.size, (0, 0, 0, 0))
-            maskImg.paste(alpha_blur_large, (-margin_width, -margin_height))
-
-            # Einfügen des Screen Glows
-            background.paste(top_image, (self.x, self.y), mask=maskImg)
-
-            # Einfügen des oberen Bildes an der gewünschten Position auf dem Hintergrund
-            # background.paste(top_image, (self.x, self.y), mask=top_image)
+            # Einfügen des oberen Bildes an der gewünschten Position auf dem Hintergrund mit BlurBox
+            backgroundComp.paste(screenshot, (self.x, self.y), mask=screenshot)
 
             # Speichern des neuen Bildes
-            background.save(output_path)
+            backgroundComp.save(output_path)
 
             if keepScreenshot is not True:
                 self.removeScreenshotTemp()
+
+    def transformScreenshot(self, image):
+        # Berechne die Höhe des oberen Bildes basierend auf dem Seitenverhältnis und der Breite
+        width_percent = (self.width / float(image.size[0]))
+        height = int((float(image.size[1]) * float(width_percent)))
+
+        # Skaliere das obere Bild auf die gewünschte Größe
+        newScreenSize = image.resize((self.width, height))
+        return newScreenSize
+
+    def add_blur(self,background,screenshot,blur_radius=100, opacity=1.0):
+
+        # Create a new image with the same size as the original image
+        rectangle = Image.new("RGBA", background.size, (255, 255, 255, 0))
+
+        # Draw the white rectangle
+        draw = ImageDraw.Draw(rectangle)
+        draw.rectangle([self.x, self.y, self.x + screenshot.width, self.y + screenshot.height], fill=(255, 255, 255, (int)(opacity * 255)))
+
+        # Blur the edges of the rectangle
+        blurBox = rectangle.filter(ImageFilter.GaussianBlur(blur_radius))
+
+        # Place the blurred rectangle onto the original image
+        result = Image.alpha_composite(background, blurBox)
+
+        return result
 
     def removeScreenshotTemp(self):
         os.remove(self.top_image_path)
