f694a9926248f976404d19f639f7182928d83f31
Sönke Tenckhoff <sotenck@Sonkes-MacBook-Pro.local>
2023-05-16 22:42:54 +0200

    added test image generator

--- imageTest.py
+++ imageTest.py
@@ -1,148 +1,16 @@
-import os
 
-import cv2
-
-from libs.bColor import bcolors
 from libs.testGen import generateTestSquare
 from libs.imageFusion import TransparentImageOverlay
 
 
-# set image creation runs
-count = 100
 
-# Create test folder
-myPath = f"output/_test/run-X{count}-V"
-
-prefixNbr = 1
-folder = myPath+f"{prefixNbr}/"
-
-while True:
-    if not os.path.isdir(folder):
-        print("folder doesn't exist", folder)
-        os.makedirs(folder)
-        break
-
-    # check if folder maybe empty
-    if len(os.listdir(folder)) == 0:
-        print("folder is empty -> use it", folder)
-        break
-
-    prefixNbr = prefixNbr + 1
-    folder = myPath+f"{prefixNbr}/"
-
-print("created folder", folder)
-
-# generate random transformed rectangles for test purposes
-print(f"Generating {count} images...")
-testEngine = generateTestSquare(count, folder)
-images = testEngine.test_run()
-print(f"{bcolors.OKGREEN}...successful{bcolors.ENDC}")
-
-# initialize testing class
-imageAnalyzer = TransparentImageOverlay("", "")
-
-blue_c = (255,0,0)
-red_c = (0,0,255)
-
-# Create Demo Image Test method with image Analyzer
-def hughPointLinecheck(contour, background):
-    cv2.imwrite(folder + f"{x}-1-canny.png", contour)
-
-    lines = imageAnalyzer.get_hough_lines(contour)
-    mapped_lines_x, mapped_lines_y = imageAnalyzer.combine_overlapping_lines(lines)
-
-    mapped_background = background.copy()
-    final_border = background.copy()
-
-    def print_lines(lines_data, image, color=blue_c):
-        if lines_data is not None:
-            for lin in lines_data:
-                x1, y1, x2, y2 = lin[0]
-                cv2.line(image, (x1, y1), (x2, y2), color, 3)
-                cv2.circle(image, (x1, y1), 15, color, 3)
-                cv2.circle(image, (x2, y2), 15, color, 3)
-
-    def print_dots(dot_data, image, color=blue_c):
-        if dot_data is not None:
-            for dot in dot_data:
-                x, y, = dot
-                cv2.circle(image, (x, y), 15, color, 3)
-
-    if lines is not None:
-        print(f"PointCheck: found {len(lines)} lines in Image")
-        print_lines(lines, background)
-    else:
-        print("PointCheck: No contour was found on the image")
-        return False
-
-    cv2.imwrite(folder + f"{x}-2-coord.png", background)
-
-    if mapped_lines_x is not None:
-        print(f"PointCheck: mapped to {bcolors.OKCYAN}{len(mapped_lines_x)} horizontal lines{bcolors.ENDC} in Image")
-        print_lines(mapped_lines_x, mapped_background)
-    else:
-        return False
-
-    if mapped_lines_y is not None:
-        print(f"PointCheck: mapped to {bcolors.OKCYAN}{len(mapped_lines_y)} vertical lines{bcolors.ENDC} in Image")
-        print_lines(mapped_lines_y, mapped_background, red_c)
-    else:
-        return False
-
-    intersect_color = (255, 255, 0)
-    # Find relevant lines from vertical/horizontal mapped
-    # Sort Lines by Central Cross-Section
-    sorted_y_intersection, vert_line = imageAnalyzer.get_relevant_lines(mapped_lines_x, True)
-    if len(sorted_y_intersection) < 2:
-        print(f"{bcolors.FAIL}Could not find any horizontal mapped lines -> abort{bcolors.ENDC}")
-        return False
-    y_points, y_lines = zip(*sorted_y_intersection)
-
-    # print intersections with vertical reference line on mapped_background
-    print_dots(y_points, mapped_background, intersect_color)
-    print_lines([(vert_line,)], mapped_background, intersect_color)
-
-    sorted_x_intersection, hori_line = imageAnalyzer.get_relevant_lines(mapped_lines_y, False)
-    if len(sorted_x_intersection) < 2:
-        print(f"{bcolors.FAIL}Could not find any vertical mapped lines -> abort{bcolors.ENDC}")
-        return False
-    x_points, x_lines = zip(*sorted_x_intersection)
-
-    # print intersections with horizontal reference line on mapped_background
-    print_dots(x_points, mapped_background, intersect_color)
-    print_lines([(hori_line,)], mapped_background, intersect_color)
-
-    cv2.imwrite(folder + f"{x}-3-mapped.png", mapped_background)
-
-    four_borders = [y_lines[0], x_lines[-1], y_lines[-1], x_lines[0]]
-    # print 4-found lines - with Intersection points
-    print_lines(four_borders, final_border)
-    cv2.imwrite(folder + f"{x}-4-reduced.png", final_border)
-
-    transform_points = imageAnalyzer.get_screen_position(four_borders)
-
-    if transform_points is not None and len(transform_points) >= 4:
-        print_dots(transform_points, final_border, (255,255,255))
-        cv2.imwrite(folder + f"{x}-4-reduced.png", final_border)
-    else:
-        print(f"{bcolors.FAIL}Could not find any intersections{bcolors.ENDC}")
-        return False
+#photoTest2 = TransparentImageOverlay(
+    #bottom_image_path="./src/mockup/iphone-14-pro-02.png",
+    #bottom_image_path="./src/mockup/macbook-air-m1-03.png",
+#    bottom_image_path="./src/mockup/xdr-display-02.png",
+#    top_image_path=""
+#)
+#photoTest2.overlay_images(newPath, "demo", keepScreenshot=False)
 
 
-
-    print("PointCheck: Saved report image")
-    return True
-
-
-successCount = 0
-for (x,data) in enumerate(images):
-    # try to find solution
-    print(f"\nAnalyze image {x + 1}/{count}")
-
-    canny = data.get("canny")
-    bg = data.get("original")
-
-    if hughPointLinecheck(canny, bg):
-        successCount += 1
-
-print(f"\n\nAnalysed with Rate of {successCount} / {count}")
+generateTestSquare(20)
--- libs/testGen.py
+++ libs/testGen.py
@@ -1,82 +1,67 @@
-import random
-
 import cv2
 import numpy as np
 
 class generateTestSquare:
 
-    def __init__(self, count, path="output/test/"):
-        self.outputPath = path
-        self.count = count
+    outputPath = "output/test/"
 
-    def test_run(self):
-        img_arr = []
-        for x in range(self.count):
-            contour, reference = self.generateDemoCanny(np.random.randint(1500, 3000), np.random.randint(1500,3000))
-            #cv2.imwrite(f"{self.outputPath}{x}.png", newTestImage)
+    def __init__(self, count):
 
-            img_arr.append({
-                "canny": contour,
-                "original": reference
-            })
+        for x in range(count):
+            newRectangle = self.generateDemoCanny(np.random.randint(1500, 3000), np.random.randint(1500,3000))
+            cv2.imwrite(f"{self.outputPath}src/{x}.png", newRectangle)
 
-        return img_arr
 
     def generateDemoCanny(self, width, height):
         # Create a black background image
         image = np.zeros((height, width, 3), dtype=np.uint8)
-        white_ref = np.ones((height, width, 3), dtype=np.uint8) * 155
 
         r_pos, r_size, r_contour = self.create_round_rectangle(width, height)
 
-        if r_pos[0] % 2 == 0:   # 50% with notch
+        if r_pos[0] % 9 == 0:
             # Sometimes create a Notch onto r_contour
             n_pos, n_size, n_contour = self.create_round_rectangle(*r_size)
             r_contour[n_pos[1]:n_pos[1] + n_size[1], n_pos[0]:n_pos[0] + n_size[0]] = n_contour
 
-        # perspectevly move two coordinates of the screen-edge
+        #perspectevly move two coordinates of the screen-edge
         transform_coordinates = self.randomTransform(
             top_left=r_pos,
             bottom_right=(r_pos[0]+r_size[0], r_pos[1]+r_size[1]),
-            max=(min(r_size) // 6)
+            max = min(r_size) // 10
         )
 
         transform_screen = self.fourWayTransform(r_contour, (width, height), transform_coordinates)
 
-        canny = cv2.cvtColor(self.overlay(image, transform_screen), cv2.COLOR_BGR2GRAY)
-        reference = self.overlay(white_ref, transform_screen)
-
-        return canny, reference
+        return self.overlay(image, transform_screen)
 
     def fourWayTransform(self, screen, bg_size, fourPointDst):
         # Define the four corner points in the source image
         # Calculate the 4-point transformation
         pts_src = np.array([
-            [0, 0],[screen.shape[1], 0],
-            [screen.shape[1], screen.shape[0]],
-            [0, screen.shape[0]]
-        ], dtype=np.float32)
+                        [0, 0],[screen.shape[1], 0],
+                        [screen.shape[1], screen.shape[0]],
+                        [0, screen.shape[0]]
+                    ], dtype=np.float32)
 
         h, status = cv2.findHomography(pts_src, np.array([*fourPointDst], dtype=np.float32))
         return cv2.warpPerspective(screen, h, bg_size)
 
     def overlay(self, image1, image2):
         for c in range(0, 3):
-            image2[:, :, c] = (image1[:, :, c] + image2[:, :, c])
+            image2[:, :, c] = (image1[:, :, c] + image2[:, :, c]) // 2
 
         return image2
 
     def randomTransform(self, top_left, bottom_right, max):
 
-        opposite_edge = bool(random.getrandbits(1))
-        x1_trans, y1_trans, x2_trans, y2_trans = np.random.randint(-max, max, size=4)
+        x_trans, y_trans = np.random.randint(-max, max), np.random.randint(-max, max)
 
-        top_left = top_left if opposite_edge else (top_left[0] + x1_trans, top_left[1] + y1_trans)
-        top_right = (bottom_right[0] + x1_trans, top_left[1] + y1_trans) if opposite_edge else (bottom_right[0], top_left[1])
-        bottom_right = bottom_right if opposite_edge else (bottom_right[0] + x2_trans, bottom_right[1] + y2_trans)
-        bottom_left = (top_left[0] - x2_trans, bottom_right[1] - y2_trans) if opposite_edge else (top_left[0], bottom_right[1])
-
-        return top_left, top_right, bottom_right, bottom_left
+        return (
+            top_left,
+            (bottom_right[0] + x_trans, top_left[1] + y_trans), # top_right
+            bottom_right,
+            (top_left[0] - x_trans, bottom_right[1] - y_trans)  # bottom_left
+        )
 
     def create_round_rectangle(self, width, height):
 
@@ -91,6 +76,7 @@ class generateTestSquare:
 
         # Create black backdrop
         screen_contour = np.zeros((rect_height, rect_width, 3), dtype=np.uint8)
+        screen_contour[:, :, 2] = 150
 
         thickness = 5
         b = thickness+1  # border
